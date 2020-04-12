cd569711d75e2c997cff74a263f93d26a49b33de
Kelvin Brito <britok30@gmail.com>
2020-04-12 11:15:01 -0400

    remove react bootstrap

--- src/components/VideoGame/SingleGame.js
+++ src/components/VideoGame/SingleGame.js
@@ -1,28 +1,25 @@
 import React, { Fragment } from "react";
 import "./Game.css";
-import { Card } from "react-bootstrap";
 
 const Game = (props) => {
-    const { name, cover, rating, date, slug, meta} = props;
+    const { name, cover, rating, date, slug, meta } = props;
     return (
         <Fragment>
-            <Card className="game-card">
-                <Card.Img variant="top" src={cover} className="game-cover" />
-                <Card.Body>
-                    <Card.Title className="title">{name}</Card.Title>
-                    <Card.Text className="detail">
-                        Release Date: {date}
-                    </Card.Text>
-                    <Card.Text className="detail">
+            <div class="card game-card">
+                <img
+                    className="card-img-top game-cover"
+                    src={cover}
+                    alt="game-cover"
+                />
+                <div className="card-body">
+                    <h5 className="card-title title">{name}</h5>
+                    <p className="card-text detail">Release Date: {date}</p>
+                    <p className="card-text detail">
                         Rating: {rating ? rating : "N/A"}
-                    </Card.Text>
-                    <Card.Text className="detail">
+                    </p>
+                    <p className="card-text detail">
                         Metacritic: {meta ? meta : "N/A"}
-                    </Card.Text>
-                    <Card.Text className="detail">
-
-                    </Card.Text>
-
+                    </p>
                     <a
                         className="more-info"
                         href={`https://rawg.io/games/${slug}`}
@@ -31,7 +28,6 @@ const Game = (props) => {
                     >
                         More Info
                     </a>
-
                     <a
                         className="screenshots"
                         href={`https://rawg.io/games/${slug}/screenshots`}
@@ -40,8 +36,8 @@ const Game = (props) => {
                     >
                         Screenshots
                     </a>
-                </Card.Body>
-            </Card>
+                </div>
+            </div>
         </Fragment>
     );
 };
