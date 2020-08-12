60a7a4005f4a948f2db61979a25940b03d8bda02
Kelvin Brito <britok30@gmail.com>
2020-08-12 00:17:34 -0400

    refactor to hooks

--- src/components/LastMonth/LastMonth.js
+++ src/components/LastMonth/LastMonth.js
@@ -1,76 +1,62 @@
-import React, { Component, Fragment } from "react";
-import axios from "axios";
-import SingleGame from "../VideoGame/SingleGame";
-import "../VideoGame/Game.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SingleGame from '../VideoGame/SingleGame';
+import '../VideoGame/Game.css';
 
-class LastMonthGames extends Component {
-    state = {
-        games: [],
-        from: "2020-05-01",
-        to: "2020-05-31",
-        per: 25,
-        platforms: '18, 1, 7' //PS4: 18, Nintendo Switch: 7, Xbox One: 1
-    };
+const LastMonthGames = () => {
+    const [games, setGames] = useState([]);
+    const [from, setFrom] = useState('2020-07-01');
+    const [to, setTo] = useState('2020-07-31');
+    const [per, setPer] = useState(25);
+    const [platforms, setPlatforms] = useState('18, 1, 7');
 
-    fetchGames = async () => {
-        const { from, to, per, platforms } = this.state;
-        this.setState({ page: this.state.page + 1 });
+    useEffect(() => {
+        fetchGames();
+    }, []);
+
+    const fetchGames = async () => {
         await axios
             .get(
                 `https://cors-anywhere.herokuapp.com/https://rawg.io/api/games?dates=${from},${to}&platforms=${platforms}&page_size=${per}`,
                 {
                     headers: {
-                        "Access-Control-Allow-Origin": "*",
+                        'Access-Control-Allow-Origin': '*',
                     },
                 }
             )
             .then((res) => {
                 console.log(res.data.results);
-                this.setState({
-                    games: this.state.games
-                        .concat(res.data.results)
-                        .sort(this.randomize),
-                });
+                setGames(games.concat(res.data.results).sort(randomize));
             });
     };
 
-    randomize = (a) => {
+    const randomize = (a) => {
         return Math.random() - 0.5;
     };
 
-    componentDidMount = async () => {
-        await this.fetchGames();
-    };
-
-    render() {
-        const { games } = this.state;
-
+    const renderGames = games.map((game) => {
         return (
-            <Fragment>
-                <h1 className="lead-head">Last Month Releases</h1>
-                <div className="card-columns">
-                    {!games ? (
-                        <h1>No games found.</h1>
-                    ) : (
-                        games.map((game) => {
-                            return (
-                                <SingleGame
-                                    key={game.id}
-                                    name={game.name}
-                                    description={game.description}
-                                    cover={game.background_image}
-                                    rating={game.rating}
-                                    meta={game.metacritic}
-                                    date={game.released}
-                                    slug={game.slug}
-                                />
-                            );
-                        })
-                    )}
-                </div>
-            </Fragment>
+            <SingleGame
+                key={game.id}
+                name={game.name}
+                description={game.description}
+                cover={game.background_image}
+                rating={game.rating}
+                meta={game.metacritic}
+                date={game.released}
+                slug={game.slug}
+            />
         );
-    }
-}
+    });
+
+    return (
+        <div>
+            <h1 className="lead-head">Last Month Releases</h1>
+            <div className="card-columns">
+                {!games ? <h1>No games found.</h1> : renderGames}
+            </div>
+        </div>
+    );
+};
 
 export default LastMonthGames;
