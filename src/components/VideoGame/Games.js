46dba74130dc6eee22b3c27747cb2805ef3ed49b
Kelvin Brito <britok30@gmail.com>
2020-08-11 23:36:38 -0400

    refactor to hooks

--- src/components/VideoGame/Games.js
+++ src/components/VideoGame/Games.js
@@ -1,78 +1,70 @@
-import React, { Component, Fragment } from "react";
-import axios from "axios";
-import SingleGame from "./SingleGame";
-import "./Game.css";
-import spinner from "../images/Spinner-1s-200px.gif";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import SingleGame from './SingleGame';
+import './Game.css';
+import spinner from '../images/Spinner-1s-200px.gif';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-class Games extends Component {
-    state = {
-        games: [],
-        page: 1,
-        per: 39,
-    };
+const Games = () => {
+    const [games, setGames] = useState([]);
+    const [pages, setPages] = useState(1);
+    const [per, setPer] = useState(39);
 
-    fetchGames = async () => {
-        const { per, page } = this.state;
-        this.setState({ page: this.state.page + 1 });
+    useEffect(() => {
+        fetchGames();
+    }, []);
+
+    const fetchGames = async () => {
+        setPages(pages + 1);
+
         await axios
             .get(
-                `https://cors-anywhere.herokuapp.com/https://rawg.io/api/games?page=${page}&page_size=${per}`,
+                `https://cors-anywhere.herokuapp.com/https://rawg.io/api/games?page=${pages}&page_size=${per}`,
                 {
                     headers: {
-                        "Access-Control-Allow-Origin": "*",
+                        'Access-Control-Allow-Origin': '*',
                     },
                 }
             )
             .then((res) => {
                 console.log(res.data.results);
-                this.setState({
-                    games: this.state.games
-                        .concat(res.data.results)
-                        .sort(this.randomize),
-                });
+                setGames(games.concat(res.data.results).sort(randomize));
             });
     };
 
-    randomize = (a) => {
+    const randomize = (a) => {
         return Math.random() - 0.5;
     };
 
-    componentDidMount = async () => {
-        await this.fetchGames();
-    };
-
-    render() {
-        const { games } = this.state;
-
+    const renderGames = games.map((game) => {
         return (
-            <Fragment>
-                <h1 className="lead-head">PlaySpace</h1>
-                <InfiniteScroll
-                    className="card-columns"
-                    dataLength={games.length}
-                    next={this.fetchGames}
-                    hasMore={true}
-                    loader={<img className="spinner" src={spinner} alt="spinner"/>}
-                >
-                    {games.map((game) => {
-                        return (
-                            <SingleGame
-                                key={game.id}
-                                name={game.name}
-                                description={game.description}
-                                cover={game.background_image}
-                                rating={game.rating}
-                                meta={game.metacritic}
-                                date={game.released}
-                                slug={game.slug}
-                            />
-                        );
-                    })}
-                </InfiniteScroll>
-            </Fragment>
+            <SingleGame
+                key={game.id}
+                name={game.name}
+                description={game.description}
+                cover={game.background_image}
+                rating={game.rating}
+                meta={game.metacritic}
+                date={game.released}
+                slug={game.slug}
+            />
         );
-    }
-}
+    });
+
+    return (
+        <Fragment>
+            <h1 className="lead-head">PlaySpace</h1>
+            <InfiniteScroll
+                className="card-columns"
+                dataLength={games.length}
+                next={fetchGames}
+                hasMore={true}
+                loader={<img className="spinner" src={spinner} alt="spinner" />}
+            >
+                {renderGames}
+            </InfiniteScroll>
+        </Fragment>
+    );
+};
 
 export default Games;
